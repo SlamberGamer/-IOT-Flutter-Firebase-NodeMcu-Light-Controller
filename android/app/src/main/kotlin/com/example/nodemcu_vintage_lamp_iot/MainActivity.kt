c584a55454b1e3464b0051d6499c1ef3461d9995
SlamberGamer <slambergamer@gmail.com>
2022-05-01 01:59:37 +0800

    Initial Files

---
+++ android/app/src/main/kotlin/com/example/nodemcu_vintage_lamp_iot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nodemcu_vintage_lamp_iot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage();
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
